9492c4bea180a2480b491ff08d17c717f0580701
Gmagnel Talang Emmanuel <83882744+Gmagnel@users.noreply.github.com>
2021-08-20 05:37:51 +0000

    Kelvin weather project

---
+++ kelvinweather.js
@@ -0,0 +1,21 @@
+
+//assigning constant value for kelvin
+const kelvin = 0;
+
+//subtracting celcius from kelvin
+var celcius;
+celcius =kelvin-273;
+var fathrenheit;
+
+// claculating the fathrenheit
+fathrenheit =celcius*(9/5)+32;
+
+//using the floor() method to round the decimal
+
+fath=Math.floor(fathrenheit);
+console.log( `The tmperature is ${fath} degrees fathrenheit`);
+
+// claculating the newton
+let newton =celcius*(33/100);
+newtont=Math.floor(newton );
+console.log( `The tmperature is ${newton } degrees newton`);
